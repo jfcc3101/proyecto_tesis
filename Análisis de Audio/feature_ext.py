13d0e88e00a9dc075f00e634c127eac449a71175
jfcc3101 <30846402+jfcc3101@users.noreply.github.com>
2019-08-21 19:36:41 -0500

    Update feature_ext.py
    Agregadas dos caracteristicas extraidas:
    - RMS Energy
    - Onset Strength

--- Análisis de Audio/feature_ext.py
+++ Análisis de Audio/feature_ext.py
@@ -10,8 +10,8 @@ y, sr = librosa.load(filename)
 onset_env = librosa.onset.onset_strength(y, sr=sr)
 
 #Promedio de Tempo
-tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
-print(tempo[0])
+tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)[0]
+print(tempo)
 
 #Témpo Dinámico
 dtempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr, aggregate=None)
@@ -32,16 +32,44 @@ plt.plot(time,dtempo)
 plt.show()
 
 #Chromagrama
+chroma = librosa.feature.chroma_stft(y=y, sr=sr)
 chroma_cens = librosa.feature.chroma_cens(y=y, sr=sr)
 chroma_cq = librosa.feature.chroma_cqt(y=y, sr=sr)
 
 plt.figure()
+librosa.display.specshow(chroma, y_axis='chroma', x_axis='time')
+plt.title('chroma')
+plt.colorbar()
+plt.figure()
 librosa.display.specshow(chroma_cq, y_axis='chroma', x_axis='time')
 plt.title('chroma_cq')
 plt.colorbar()
-#plt.subplot(2,1,2)
-#librosa.display.specshow(chroma_cens, y_axis='chroma', x_axis='time')
-#plt.title('chroma_cens')
-#plt.colorbar()
-#plt.tight_layout()
+plt.figure()
+librosa.display.specshow(chroma_cens, y_axis='chroma', x_axis='time')
+plt.title('chroma_cens')
+plt.colorbar()
+plt.tight_layout()
+plt.show()
+
+#Energía
+librosa.feature.rmse(y=y)
+S, phase = librosa.magphase(librosa.stft(y))
+rms = librosa.feature.rmse(S=S)
+plt.figure()
+plt.semilogy(rms.T, label='RMS Energy')
+plt.xticks([])
+plt.xlim([0, rms.shape[-1]])
+plt.legend()
+plt.show()
+
+#Tempograma
+tempogram = librosa.feature.tempogram(onset_envelope=onset_env, sr=sr, hop_length=512)
+#ac_global = librosa.autocorrelate(onset_env, max_size=tempogram.shape[0])
+#ac_global = librosa.util.normalize(ac_global)
+#tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr,hop_length=512)[0]
+plt.figure()
+plt.plot(onset_env, label='Onset Strength')
+plt.xticks([])
+plt.legend(frameon=True)
+plt.axis('tight')
 plt.show()
