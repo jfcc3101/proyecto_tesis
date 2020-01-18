4cf0af94d54716037cbbdc4e8a193b63bc05d1c5
jfcc3101 <jfcc3101@users.noreply.github.com>
2020-01-18 18:15:13 -0500

    Avances en el manejo de XML
    Se permite la conversión a arreglo de flotantes en el proyecto PruebaXML

--- PruebaXML/Control.gd
+++ PruebaXML/Control.gd
@@ -20,12 +20,29 @@ func _on_Boton_pressed():
 	#print(path)
 	
 	print(len(cancion))
-	print(cancion[4])
-	cancion[2][0] = ""
+	print("Cancion: " + cancion[0])
+	print("Tempo Medio: " + cancion[1])
+	#cancion[5] = cancion[5].replace("[","")
+	#cancion[5] = cancion[5].rsplit(" ")
+	cancion[1] = float(cancion[1])
+	for i in range(2,len(cancion)-1):
+		cancion[i] = cancion[i].replace("[","")
+		cancion[i] = cancion[i].rsplit(" ")
+		print(cancion[i][0])
+	for i in range(2,len(cancion)-1):
+		cancion[i] = Array(cancion[i])
+		for k in range(0,len(cancion[i])-1):
+			#cancion[i][k] = str(cancion[i][k])
+			cancion[i][k] = cancion[i][k].to_float()
+		
+	print("suma" + str(cancion[3][0] + cancion[4][0]))
+	#cancion[2][0] = ""
 	#cancion[2].erase(0, '[')
-	var tempo = cancion[2].split(" ")
+	"""var tempo = Array(cancion[2])
+	for l in range(1,len(tempo)-1):
+		tempo[l] = float(tempo[l])"""
 	
-	print(tempo[200])
+	#print(tempo[200])
 	#for i in range(0,len(cancion)):
 	#	print(cancion[i])
 	pass
