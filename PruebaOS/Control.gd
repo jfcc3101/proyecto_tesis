b818e6fd829bc86966c62a83efae7d807df6c000
Javier Castaneda <jfcc3101@outlook.com>
2020-06-11 15:13:58 -0500

    Intento de ejecución en Godot

--- PruebaOS/Control.gd
+++ PruebaOS/Control.gd
@@ -3,7 +3,8 @@ extends Control
 #var parser = XMLParser.new()
 #var cancion = Array()
 # Called when the node enters the scene tree for the first time.
-var args = ["python feature_ext.py"]
+#var args = ["feature_ext.py"]
+var args = ["runfile('D:/Documentos/GitHub/proyecto_tesis/PruebaOS/feature_ext.py'), wdir='D:/Documentos/GitHub/proyecto_tesis/PruebaOS')"]
 func _ready():
 	
 	#print(parser.open("res://saves/actual.xml"))
@@ -13,5 +14,6 @@ func _ready():
 
 
 func _on_Boton_pressed():
-	print(OS.execute("cmd.exe",args, true))
+	#print(OS.execute("C:\\ProgramData\\Anaconda3\\python.exe",args, true))
+	print(OS.execute("C:\\ProgramData\\Anaconda3\\Scripts\\spyder.exe",args, true))
 	pass
